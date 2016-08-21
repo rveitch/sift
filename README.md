244a3e8770069c16511ae16e01b9d5f2a83c4ddd
ryan.veitch <ryan.r.veitch@gmail.com>
2016-08-21 16:21:57 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sift
-SearhKit &amp; Elasticsearch powered search
+# Searchkit ES6 Boilerplate
+Small project to get started using searchkit. Webpack, es6 (es2015 babel)
+
+## Steps
+* npm install
+* npm start
+* visit http://localhost:3000
---
+++ src/SearchPage.jsx
@@ -0,0 +1,104 @@
+import * as React from "react";
+import * as _ from "lodash";
+
+import {
+	SearchkitManager, SearchkitProvider,
+	SearchBox, RefinementListFilter, MenuFilter,
+	Hits, HitsStats, NoHits, Pagination, SortingSelector,
+	SelectedFilters, ResetFilters, ItemHistogramList,
+	Layout, LayoutBody, LayoutResults, TopBar,
+	SideBar, ActionBar, ActionBarRow, RangeFilter, SearchkitComponent
+} from "searchkit";
+
+require("./index.scss");
+
+const host = "https://3590b9d403c87e0697b6:8c2e5209a1@f08f4b1b.qb0x.com:30242/fccnn"
+const searchkit = new SearchkitManager(host, {
+	searchOnLoad: true,
+	useHistory: false,
+  basicAuth:"3590b9d403c87e0697b6:8c2e5209a1"
+})
+//var Hits = Searchkit.Hits;
+
+const MovieHitsGridItem = (props)=> {
+  const {bemBlocks, result} = props
+	let url = "http://" + result._source.url
+  let thumb = "http://" + result._source.search_thumb
+	let img = (thumb == "http://null") ? "http://www.inforum.com/sites/all/themes/inforum_theme/images/touch-icon.png":"http://" + result._source.search_thumb
+  const source:any = _.extend({}, result._source, result.highlight)
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <a href={url} target="_blank">
+        <img data-qa="poster" className={bemBlocks.item("poster")} src={img} width="170" height="170"/>
+        <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}>
+        </div>
+      </a>
+    </div>
+  )
+}
+
+export class SearchPage extends React.Component {
+	render(){
+		return (
+			<SearchkitProvider searchkit={searchkit}>
+		    <Layout>
+		      <TopBar>
+						<div className="fcc-logo"><img data-qa="fcc-ico" className="fcc-menu-logo" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/446514/Inforum_Logo_2016_searchkit-2.png" width="166" height="40"/></div>
+		        <SearchBox
+		          autofocus={true}
+		          searchOnChange={true}
+							placeholder="Search content..."
+		          prefixQueryFields={["title^2", "body.value", "author.realname", "category.categories.name", "tags.name"]}/>
+		      </TopBar>
+		      <LayoutBody>
+		        <SideBar>
+							<MenuFilter
+								id="type"
+								title="Content Type"
+								field="type.raw"
+								listComponent={ItemHistogramList}/>
+		          <RefinementListFilter
+		            id="newspaper"
+		            title="Newspaper"
+		            field="newspaper.raw"
+		            operator="AND"
+		            size={6}/>
+							<RefinementListFilter
+		            id="category"
+		            title="Category"
+		            field="category.name"
+		            operator="AND"
+		            size={6}/>
+							<RefinementListFilter
+								id="author"
+								title="Author"
+								field="author.realname.raw"
+								operator="AND"
+								size={6}/>
+		        </SideBar>
+		        <LayoutResults>
+		          <ActionBar>
+		            <ActionBarRow>
+		              <HitsStats/>
+									<SortingSelector options={[
+										{label:"Relevance", field:"_score", order:"desc", defaultOption:true},
+										{label:"Newest", field:"created", order:"desc"},
+										{label:"Oldest", field:"created", order:"asc"}
+									]}/>
+		            </ActionBarRow>
+		            <ActionBarRow>
+		              <SelectedFilters/>
+		              <ResetFilters/>
+		            </ActionBarRow>
+		          </ActionBar>
+		          <Hits mod="sk-hits-grid" hitsPerPage={10} itemComponent={MovieHitsGridItem}
+		            sourceFilter={["title", "search_thumb", "url", "nid"]}/>
+		          <NoHits/>
+							<Pagination showNumbers={true}/>
+		        </LayoutResults>
+		      </LayoutBody>
+		    </Layout>
+		  </SearchkitProvider>
+		)
+	}
+}
