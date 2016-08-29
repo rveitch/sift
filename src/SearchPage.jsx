03d741d8e7a04b37f0b4d4faf8cbc104a7f96457
ryan.veitch <ryan.r.veitch@gmail.com>
2016-08-29 18:33:40 -0500

    Added List View / View Switcher

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -7,7 +7,8 @@ import {
 	Hits, HitsStats, NoHits, Pagination, SortingSelector,
 	SelectedFilters, ResetFilters, ItemHistogramList,
 	Layout, LayoutBody, LayoutResults, TopBar,
-	SideBar, ActionBar, ActionBarRow, RangeFilter, SearchkitComponent
+	SideBar, ActionBar, ActionBarRow, RangeFilter, ViewSwitcherToggle,
+  ViewSwitcherHits, MultiMatchQuery, SearchkitComponent
 } from "searchkit";
 
 require("./index.scss");
@@ -18,6 +19,10 @@ const searchkit = new SearchkitManager(host, {
 	useHistory: false,
   basicAuth:"3590b9d403c87e0697b6:8c2e5209a1"
 })
+
+//var myVar = setInterval(searchkit.reloadSearch(), 1000)
+
+//setInterval searchkit.reloadSearch()
 
 /*searchkit.addDefaultQuery((query)=> {
     return query.addQuery(FilteredQuery({
@@ -30,21 +35,19 @@ const searchkit = new SearchkitManager(host, {
 //http://docs.searchkit.co/stable/docs/setup/default-query.html
 
 //source.author[0].realname
+function refreshMe() {
+	//searchkit.reloadSearch()
+	//console.log('refreshed')
+}
 
 const MovieHitsGridItem = (props)=> {
   const {bemBlocks, result} = props
-
 	let url = "http://" + result._source.url
-
-
 	let post_date = new Date(result._source.post_date)
 	let date = post_date.toDateString()
 	let thumb = (result._source.search_thumb == "www.fccnn.com/sites/default/files/styles/square_300/public") ? null:result._source.search_thumb
 	let img = (thumb == null) ? "http://www.inforum.com/sites/all/themes/inforum_theme/images/touch-icon.png":"http://" + result._source.search_thumb
   const source:any = _.extend({}, result._source, result.highlight)
-	//let authorname = source.author
-	//console.log(authorname.realname);
-	//<h3 className={bemBlocks.item("subtitle")}>{date}, by {source.name}</h3>
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
       <a href={url} target="_blank">
@@ -56,8 +59,38 @@ const MovieHitsGridItem = (props)=> {
   )
 }
 
+export const MovieHitsListItem = (props)=> {
+  const {bemBlocks, result} = props
+	let url = "http://" + result._source.url
+	let post_date = new Date(result._source.post_date)
+	let date = post_date.toDateString()
+	let thumb = (result._source.search_thumb == "www.fccnn.com/sites/default/files/styles/square_300/public") ? null:result._source.search_thumb
+	let img = (thumb == null) ? "http://www.inforum.com/sites/all/themes/inforum_theme/images/touch-icon.png":"http://" + result._source.search_thumb
+	let name = (result._source.name) ? ", by " + result._source.name:""
+	let author = (result._source.author) ? result._source.author:""
+	let authorname = (author.realname) ? ", by " + author.realname:name
+	let body = result._source.body
+	let bodyval = (body.safe_value !== "") ? body.safe_value:body.value.replace(/(<([^>]+)>)/ig,"")
+	let exerpt = bodyval.substr(0, 252)+'&hellip;'
+	let category = (result._source.category[0]['name']) ? "| " + result._source.category[0]['name']:""
+  const source:any = _.extend({}, result._source, result.highlight)
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <div className={bemBlocks.item("poster")}>
+        <img data-qa="poster" src={img}/>
+      </div>
+      <div className={bemBlocks.item("details")}>
+        <a href={url} target="_blank"><h2 className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}></h2></a>
+        <h3 className={bemBlocks.item("subtitle")}>{date}{authorname} | {source.newspaper} {category}</h3>
+        <div className={bemBlocks.item("text")} dangerouslySetInnerHTML={{__html:exerpt}}></div>
+      </div>
+    </div>
+  )
+}
+
 export class SearchPage extends React.Component {
 	render(){
+		let refresh = setInterval(refreshMe, 60000)
 		return (
 			<SearchkitProvider searchkit={searchkit}>
 		    <Layout>
@@ -96,22 +129,33 @@ export class SearchPage extends React.Component {
 								size={6}/>
 		        </SideBar>
 		        <LayoutResults>
+
 		          <ActionBar>
 		            <ActionBarRow>
 		              <HitsStats/>
+									<ViewSwitcherToggle/>
 									<SortingSelector options={[
 										{label:"Newest", field:"created", order:"desc", defaultOption:true},
 										{label:"Relevance", field:"_score", order:"desc"},
 										{label:"Oldest", field:"created", order:"asc"}
 									]}/>
 		            </ActionBarRow>
+
 		            <ActionBarRow>
 		              <SelectedFilters/>
 		              <ResetFilters/>
 		            </ActionBarRow>
 		          </ActionBar>
-		          <Hits mod="sk-hits-grid" hitsPerPage={16} itemComponent={MovieHitsGridItem}
-		            sourceFilter={["title", "search_thumb", "url", "nid", "post_date", "author.realname", "name"]}/>
+
+							<ViewSwitcherHits
+								hitsPerPage={16}
+								sourceFilter={["title", "search_thumb", "url", "nid", "post_date", "author", "name", "newspaper", "body", "category"]}
+								hitComponents = {[
+									{key:"grid", title:"Grid", itemComponent:MovieHitsGridItem, defaultOption:true},
+									{key:"list", title:"List", itemComponent:MovieHitsListItem}
+								]}
+								scrollTo="body"
+								/>
 		          <NoHits/>
 							<Pagination showNumbers={true}/>
 		        </LayoutResults>
